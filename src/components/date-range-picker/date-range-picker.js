962d618eae322b7857c33de70f72c2366982dc2e
Rao Mudasir <raomuhammad.mudasir@ceative.co.uk>
2023-06-06 17:47:26 +0500

    push

--- src/components/date-range-picker/date-range-picker.js
+++ src/components/date-range-picker/date-range-picker.js
@@ -1,33 +1,37 @@
-import React, { useState } from 'react';
-import dayjs from 'dayjs';
-import { Modal, Button, Typography } from '@mui/material';
-import CloseIcon from '@mui/icons-material/Close';
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import React, { useState } from "react";
+import dayjs from "dayjs";
+import { Modal, Button, Typography } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
-const DateRangePicker = ({ isOpenDatePicker, onCloseDatePicker, onApplyDate }) => {
+const DateRangePicker = ({
+  isOpenDatePicker,
+  onCloseDatePicker,
+  onApplyDate,
+}) => {
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
   const [error, setError] = useState(null);
 
   const handleDateChange = (date, label) => {
-    if (label === 'Start date') {
-        setStartDate(date);
-      } else if (label === 'End Date') {
-        setEndDate(date);
-      }
+    if (label === "Start date") {
+      setStartDate(date);
+    } else if (label === "End Date") {
+      setEndDate(date);
+    }
   };
 
   const handleApplyClick = () => {
     if (startDate && endDate && startDate.isAfter(endDate)) {
-        setError('Please select a valid date range.');
-        return;
-      }
-  
-      onApplyDate({ startDate, endDate });
-      onCloseDatePicker();
+      setError("Please select a valid date range.");
+      return;
+    }
+
+    onApplyDate({ startDate, endDate });
+    onCloseDatePicker();
   };
 
   const handleClearClick = () => {
@@ -40,85 +44,90 @@ const DateRangePicker = ({ isOpenDatePicker, onCloseDatePicker, onApplyDate }) =
     <Modal
       open={isOpenDatePicker}
       onClose={onCloseDatePicker}
-      BackdropProps={{ style: { backgroundColor: 'transparent', } }}
+      BackdropProps={{ style: { backgroundColor: "transparent" } }}
       style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        position: 'absolute',
-        bottom: '16px',
-        right: '16px',
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        position: "absolute",
+        bottom: "16px",
+        right: "16px",
       }}
     >
       <div
         style={{
-          height: '316px',
-          width: '592px',
-          borderRadius: '8px',
-          background: '#FFFFFF',
-          boxShadow: '0px 0px 16px rgba(0, 0, 0, 0.16)',
-          padding: '16px',
-          position: 'relative',
+          height: "316px",
+          width: "592px",
+          borderRadius: "8px",
+          background: "#FFFFFF",
+          boxShadow: "0px 0px 16px rgba(0, 0, 0, 0.16)",
+          padding: "16px",
+          position: "relative",
         }}
       >
         <div
           style={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            marginBottom: '16px',
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginBottom: "16px",
           }}
         >
-          <h3 style={{ color: '#000000', margin: 0 }}>Filters</h3>
+          <h3 style={{ color: "#000000", margin: 0 }}>Filters</h3>
           <CloseIcon
             onClick={onCloseDatePicker}
-            style={{ cursor: 'pointer', color: '#000000' }}
+            style={{ cursor: "pointer", color: "#000000" }}
           />
         </div>
-        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start' }}>
-  <Typography style={{ flex: '1',marginLeft:'2px'}}>From</Typography>
-  <Typography style={{ flex: '1' }}>To</Typography>
-</div>
-
-        
-       <div style={{marginTop:'20px'}}>
-       <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DemoContainer components={['DatePicker', 'DatePicker']}>
-          
-            <DatePicker
-              label="Start date"
-              variant='standared'
-              value={startDate}
-              onChange={(date) => handleDateChange(date, 'Start date')}
-            />
-            <DatePicker
-              label="End Date"
-              value={endDate}
-              onChange={(date) => handleDateChange(date, 'End Date')}
-            />
-          </DemoContainer>
-        </LocalizationProvider>
-       </div>
-         <Typography style={{ color: 'red', marginBottom: '8px' }}>{error}</Typography>
         <div
           style={{
-            position: 'absolute',
-            bottom: '16px',
-            right: '16px',
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "flex-start",
           }}
         >
-           
+          <Typography style={{ flex: "1", marginLeft: "2px" }}>From</Typography>
+          <Typography style={{ flex: "1" }}>To</Typography>
+        </div>
+
+        <div style={{ marginTop: "20px" }}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DemoContainer components={["DatePicker", "DatePicker"]}>
+              <DatePicker
+                label="Start date"
+                variant="standared"
+                value={startDate}
+                onChange={(date) => handleDateChange(date, "Start date")}
+              />
+              <DatePicker
+                label="End Date"
+                value={endDate}
+                onChange={(date) => handleDateChange(date, "End Date")}
+              />
+            </DemoContainer>
+          </LocalizationProvider>
+        </div>
+        <Typography style={{ color: "red", marginBottom: "8px" }}>
+          {error}
+        </Typography>
+        <div
+          style={{
+            position: "absolute",
+            bottom: "16px",
+            right: "16px",
+          }}
+        >
           <Button
             variant="outlined"
             color="primary"
             onClick={handleClearClick}
             sx={{
-              color: '#000000',
-              borderColor: '#000000',
-              marginRight: '8px',
-              '&:hover': {
-                borderColor: '#000000',
-                color: '#000000',
+              color: "#000000",
+              borderColor: "#000000",
+              marginRight: "8px",
+              "&:hover": {
+                borderColor: "#000000",
+                color: "#000000",
               },
             }}
           >
@@ -130,11 +139,11 @@ const DateRangePicker = ({ isOpenDatePicker, onCloseDatePicker, onApplyDate }) =
             onClick={handleApplyClick}
             disabled={!startDate || !endDate}
             sx={{
-              color: '#FFFFFF',
-              background: '#000000',
-              '&:hover': {
-                background: '#000000',
-                color: '#FFFFFF',
+              color: "#FFFFFF",
+              background: "#000000",
+              "&:hover": {
+                background: "#000000",
+                color: "#FFFFFF",
               },
             }}
           >
--- src/features/workflow-success-speedometer/workflow-success-speedometer.jsx
+++ src/features/workflow-success-speedometer/workflow-success-speedometer.jsx
@@ -5,25 +5,25 @@ import "./workflow-success-speedometer.scss";
 export const WorkflowSuccessSpeedometer = () => {
     return (
         <>
-        <div className="workflow-speedometer flex justify-center margin-top-2">
-
-            <Box className="position-relative" sx={{ width: '534px', height: '334px' }}>
-                <div className="circle"></div>
-                <ReactSpeedometer
-                    value={75}
-                    maxSegmentLabels={4}
-                    segments={1000}
-                    minValue={0}
-                    maxValue={100}
-                    needleColor="#C4C4CC"
-                    fluidWidth
-                    needleHeightRatio={0.7}
-                    ringWidth={27}
-                    needleTransition="easeElastic"
-                    needleTransitionDuration={3333}
-                />
-            </Box>
-        </div>
+            <div className="workflow-speedometer flex justify-center margin-top-2">
+                <Box className="position-relative" sx={{ width: { sm: '534px', xs: '320px' }, height: '334px' }}>
+                    <div className="circle"></div>
+                    <ReactSpeedometer
+                        value={75}
+                        maxSegmentLabels={4}
+                        segments={1000}
+                        minValue={0}
+                        currentValueText=""
+                        maxValue={100}
+                        needleColor="#C4C4CC"
+                        fluidWidth
+                        needleHeightRatio={0.7}
+                        ringWidth={27}
+                        needleTransition="easeElastic"
+                        needleTransitionDuration={3333}
+                    />
+                </Box>
+            </div>
         </>
     )
 }
