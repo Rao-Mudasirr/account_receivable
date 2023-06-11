1b83eda7abcc20d46c5b17867ee905c24d18fb6e
ch-fahad <muhammad.fahad@ceative.co.uk>
2023-06-11 20:03:56 +0500

    signin component

---
+++ src/components/signin/Signin.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import SignInForm from './SignInForm';
+import './signin.css'
+import { Box, Card } from '@mui/material';
+
+const Signin = () => {
+  return (
+    <div className='signin-main_container'>
+      <Box className='signin_container'>
+      <h2 style={{color:"#666666"}}>Sign In to <span style={{color:"black"}}>Accountants Pack!</span></h2>
+      <p>Welcome Back</p>
+      <SignInForm />
+      </Box>
+    </div>
+  );
+};
+
+export default ASignin
