f63374f27c6fe45f6ae47e9fb099f930a934e5cd
Rao Mudasir <raomuhammad.mudasir@ceative.co.uk>
2023-06-12 17:06:23 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Overdue_Invoice from './pages/Overdue_Invoice';
 import View_Invoice from "./pages/View_invoice_details";
 import InvoiceNoDetail from "./features/Invoices/components/invoice-no-detail/InvoiceNoDetail";
 import Manager from "./features/user-management/components/role_rights_manager/Manager";
+import Notifications from "./features/user-management/components/role_rights_manager/Accounts_receivable_data/Notifications";
 
 function App() {
   return (
@@ -18,61 +19,36 @@ function App() {
         <Route exact path={"/"} element={<Layout />}>
           <Route exact path={"/"} element={<Dashboard />} />
           <Route exact path={"/invoices"} element={<InvoiceListing />} />
+          <Route exact path={"/"} element={<Dashboard />} />
+          <Route exact path={"/invoices"} element={<InvoiceListing />} />
+          <Route exact path={"/notifications"} element={<Notifications />} />
           <Route
-          exact
-          path={"/"}
-          element={
-              <Dashboard />
-          }
-        />
-      <Route
-          exact
-          path={"/invoices"}
-          element={
-              <InvoiceListing/>
-          }
-        />
-      <Route
-          exact
-          path={"/overdue-invoices"}
-          element={
-              <Overdue_Invoice/>
-          }
-        />
-      <Route
-          exact
-          path={"/user-management/role-right-manager"}
-          element={
-              <Manager/>
-          }
-        />
-      <Route
-          exact
-          path={"/performance-analysis"}
-          element={
-              <View_Invoice />
-          }
-        />
-      
-      <Route
-          exact
-          path={"/user-management"}
-          element={
-              <UserManagements/>
-          }
-        />
-        <Route
-          exact
-          path={"/clients"}
-          element={
-              <Clientdetails/>
-          }
-        />
-            <Route path="/not-found" element={<h1>Not Found</h1>}/>
-            <Route path="*" element={<Navigate to="/not-found"/>} />
-            <Route path="/invoice-no-detail" element={<InvoiceNoDetail/>} />
-          </Route>
-    </Routes>
+            exact
+            path={"/overdue-invoices"}
+            element={<Overdue_Invoice />}
+          />
+          <Route
+            exact
+            path={"/user-management/role-right-manager"}
+            element={<Manager />}
+          />
+          <Route
+            exact
+            path={"/performance-analysis"}
+            element={<View_Invoice />}
+          />
+
+          <Route
+            exact
+            path={"/user-management"}
+            element={<UserManagements />}
+          />
+          <Route exact path={"/clients"} element={<Clientdetails />} />
+          <Route path="/not-found" element={<h1>Not Found</h1>} />
+          <Route path="*" element={<Navigate to="/not-found" />} />
+          <Route path="/invoice-no-detail" element={<InvoiceNoDetail />} />
+        </Route>
+      </Routes>
     </Suspense>
   );
 }
