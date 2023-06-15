bff7ac0095596bf12cb527dedf0b03c4424171cd
muhammad kashiff <muhammad.kashif@ceative.co.uk>
2023-06-15 21:55:07 +0500

    email

--- src/features/settings/change-email/change-email.jsx
+++ src/features/settings/change-email/change-email.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
-import { Grid, Typography,styled, TextField} from '@mui/material'
+import { Grid, Typography,styled, TextField,InputAdornment } from '@mui/material'
 import { useFormik } from "formik";
 import * as yup from "yup";
 import ProfileManagement from "../profile-management/profile-management";
 import GlobalButton from "../../../components/global-button/global-button";
-
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
 const validationSchema = yup.object({
    
-    email: yup.string().required("Email is required"),
+  password: yup.string().required("Password is required"),
+  email: yup.string().required("Email is required"),
   
   });
   
@@ -20,22 +22,37 @@ const ChangeEmail = () => {
     const [selectedDate, setSelectedDate] = useState(null);
     const [editMode, setEditMode] = useState(false);
     const [emptyFields, setEmptyFields] = useState([]);
+    const [emailTemplate,setEmailTemplate]=useState(false);
 
     const[ProfileManage,setProfileMange]=useState(false);
+    const [showPassword, setShowPassword] = useState(false);
+    const[ConfirmPassword,setConfirmPassword]=useState(true);
+    const [showEmail,setShowEmail]=useState(false);
+
 
     const handleClickUserProfile=()=>{
       setProfileMange(true);
     }
 
+    const handleUpdateEmail = () => {
+      // Display timer for 10 seconds
+      setEmailTemplate(false);
+      setTimeout(() => {
+        setEmailTemplate(true);
+      }, 10000);
+    };
+
     const formik = useFormik({
         initialValues: {
+          password:"",
           email: "",
         },
         validationSchema,
         onSubmit: (values) => {
          
           const emptyFields = [];
-          if (!values.email) emptyFields.push("email");
+          if (!values.password) emptyFields.push("password");
+          // if (!values.email) emptyFields.push("email");
         
     
           setEmptyFields(emptyFields);
@@ -43,7 +60,7 @@ const ChangeEmail = () => {
         //   if (emptyFields.length === 0) {
         //     // No empty fields, perform update
         //     toast.success("Profile updated successfully!");
-        //     setEditMode(false);
+        //    setShowEmail(true)
         //   }
         },
       });
@@ -51,11 +68,20 @@ const ChangeEmail = () => {
         formik.handleBlur(event);
       };
     
-      const handleSubmit = (event) => {
-        event.preventDefault();
-        formik.handleSubmit(event);
+      const handleSubmit = async () => {
+        // Check if the text field is filled
+        if (formik.values.password.trim() !== '') {
+          await formik.handleSubmit();
+          setConfirmPassword(false);
+        } else {
+          // Handle case when the text field is not filled
+          formik.setFieldError('password', 'Password is required');
+        }
       };
-
+      
+      
+      
+     
   return (
     <>
     {ProfileManage ? <ProfileManagement/>:(<Grid container spacing={2}>
@@ -63,47 +89,109 @@ const ChangeEmail = () => {
             <Typography>
                 Update Email
             </Typography>
-            <Typography sx={{cursor:'pointer'}} onClick={handleClickUserProfile}>
+            <Typography sx={{cursor:'pointer',color:'#166088'}} className="primary-title font-family-exo2" onClick={handleClickUserProfile}>
                 User Profile
             </Typography>
         </Grid>
         <form onSubmit={handleSubmit}>
-        <Grid item xl={12} xs={12} sx={{p:5}}>
-                <label htmlFor="email"  className="secondary-color primary-title font-family-exo2" sx={{fontWeight:400}} >
-               Current Password
-                </label>
-                <TextField
-                  id="email"
-                  name="email"
-                  size="small"
-                  value={formik.values.email}
-                  onChange={formik.handleChange}
-                  onBlur={handleBlur}
-                  autoComplete={false}
-                  // disabled={!editMode}
-                  error={formik.touched.email && Boolean(formik.errors.email)}
-                  helperText={formik.touched.email && formik.errors.email}
-                  fullWidth
-                  
-                  sx={{
-                   
-                      ".MuiTextField-root": {
-                       height:'12px',
-                       border:'1px solid black'
-                      },
+       {ConfirmPassword && <Grid item xl={12} xs={12} sx={{ p: 5 }}>
+      <label
+        htmlFor="password"
+        className="secondary-color primary-title font-family-exo2"
+        sx={{ fontWeight: 400 }}
+      >
+        Current Password
+      </label>
+      <TextField
+  id="password"
+  name="password"
+  size="small"
+  value={formik.values.password}
+  onChange={formik.handleChange}
+  onBlur={handleBlur}
+  autoComplete={false}
+  error={formik.touched.password && Boolean(formik.errors.password)}
+  helperText={formik.touched.password && formik.errors.password}
+  fullWidth
+  // type={showPassword ? 'text' : 'password'}
+  InputProps={{
+    endAdornment: (
+      <InputAdornment position="end">
+        {showPassword ? (
+          <VisibilityIcon
+            onClick={() => setShowPassword(false)}
+            sx={{ cursor: 'pointer' }}
+          />
+        ) : (
+          <VisibilityOffIcon
+            onClick={() => setShowPassword(true)}
+            sx={{ cursor: 'pointer' }}
+          />
+        )}
+      </InputAdornment>
+    ),
+  }}
+  sx={{
+    '.MuiTextField-root': {
+      height: '12px',
+      border: '1px solid black',
+    },
+  }}
+/>
 
-                  }}
-                />
-              </Grid>
+    </Grid>} 
+
+       {!ConfirmPassword && <Grid item xl={12} xs={12} sx={{ p: 5 }}>
+      <label
+        htmlFor="email"
+        className="secondary-color primary-title font-family-exo2"
+        sx={{ fontWeight: 400 }}
+      >
+        New Email
+      </label>
+      <TextField
+        id="email"
+        name="email"
+        size="small"
+        value={formik.values.email}
+        onChange={formik.handleChange}
+        onBlur={handleBlur}
+        autoComplete={false}
+        // error={formik.touched.email && Boolean(formik.errors.email)}
+        // helperText={formik.touched.email && formik.errors.email}
+        fullWidth
+        // type={showPassword ? 'text' : 'password'} v  
+        sx={{
+          '.MuiTextField-root': {
+            height: '12px',
+            border: '1px solid black',
+          },
+        }}
+      />
+    </Grid>} 
         </form>
-        <Grid xs={12} sx={{display:'flex',justifyContent:'flex-end'}}>
-              <GlobalButton
-              variant="accent"
-              btnName="accent"
-              btnText="Next"
-              sx={{width:'91px',height:'48px'}}
-              />
-              </Grid>
+       
+       
+        <Grid xs={12} sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+      {ConfirmPassword ? (
+        <GlobalButton
+          variant="accent"
+          btnName="accent"
+          btnText="Next"
+          sx={{ width: '91px', height: '48px' }}
+          onClick={() => handleSubmit()}
+        />
+      ) : (
+        <GlobalButton
+          variant="accent"
+          btnName="accent"
+          btnText="Update"
+          sx={{ width: '91px', height: '48px' }}
+          // onClick={handleUpdateEmail}
+        />
+      )}
+    </Grid>
+             
              
     </Grid>)}
     
