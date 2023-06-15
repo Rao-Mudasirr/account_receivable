5142415895f3650a692813ae91da126aff2fa0a2
Ali Hassan <ali.hassan37@ceative.co.uk>
2023-06-15 22:01:34 +0500

    Worked on Filters

--- src/pages/Report-Info.jsx
+++ src/pages/Report-Info.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReportDetails from '../features/Reports/DSO_Report/ReportDetails'
 import { useParams } from 'react-router-dom';
 import { CashCollectionReport } from '../features/Reports/cash-collection-report/cash-collection-report';
+import CashFlowForCasting from '../features/Reports/CashflowForcasting/CashFlowForCasting';
 
 const reportsPages = {
   "dso": <ReportDetails />,
-  "cashflow-forecasting-report": <ReportDetails />,
+  "cashflow-forecasting-report": <CashFlowForCasting />,
   "cash-collection-report": <CashCollectionReport />,
   "aging-buckets-report": <ReportDetails />,
   "workflows-report": <ReportDetails />,
