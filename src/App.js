0f91f244b37604dac3b28148c0278998de2e5c40
AliHassanS37 <ali.hassan91172@gmail.com>
2023-06-16 15:48:06 +0500

    Worked on Client section

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ import Overdue_Invoice from "./pages/Overdue_Invoice";
 import { Notifications } from "./pages/notifications";
 import InvoiceNoDetail from "./features/Invoices/components/invoice-no-detail/InvoiceNoDetail";
 import Manager from "./features/user-management/components/role_rights_manager/Manager";
-import Signin from './components/signin/Signin'
+import Signin from "./components/signin/Signin";
 import MainReport from "./pages/MainReport";
 import ReportInfo from "./pages/Report-Info";
 import DashboaedSettings from "./pages/dashboard-settings";
-import WorkflowReport from './features/Reports/workflow-report/WorkflowReport'
+import WorkflowReport from "./features/Reports/workflow-report/WorkflowReport";
 import WorkflowDetail from "./features/Reports/workflow-report/workflow-details/WorkflowDetail";
 import Workflow from "./pages/workflows";
 import ClientWorkFlow from "./pages/client-workflows";
@@ -29,7 +29,7 @@ function App() {
   return (
     <Suspense fallback={<h1>Loading</h1>}>
       <Routes>
-      <Route path="/signin" element={<Signin/>} />
+        <Route path="/signin" element={<Signin />} />
         <Route exact path={"/"} element={<Layout />}>
           <Route exact path={"/"} element={<Dashboard />} />
           <Route exact path={"/invoices"} element={<InvoiceListing />} />
@@ -39,24 +39,21 @@ function App() {
             path={"/overdue-invoices"}
             element={<Overdue_Invoice />}
           />
-         <Route
-          exact
-          path={"/settings"}
-          element={
-             <DashboaedSettings/>
-          }
-        />
-            <Route path="/not-found" element={<h1>Not Found</h1>}/>
-            <Route path="*" element={<Navigate to="/not-found"/>} />
-            <Route path="/invoice-no-detail" element={<InvoiceNoDetail/>} />
-            {/* <Route path="/report-details/workflows-report" element={<WorkflowReport/>} /> */}
-            <Route path="/report-details/workflows-report/workflow-detail" element={<WorkflowDetail/>} />
+          <Route exact path={"/settings"} element={<DashboaedSettings />} />
+          <Route path="/not-found" element={<h1>Not Found</h1>} />
+          <Route path="*" element={<Navigate to="/not-found" />} />
+          <Route path="/invoice-no-detail" element={<InvoiceNoDetail />} />
+          {/* <Route path="/report-details/workflows-report" element={<WorkflowReport/>} /> */}
+          <Route
+            path="/report-details/workflows-report/workflow-detail"
+            element={<WorkflowDetail />}
+          />
           <Route
             exact
             path={"/user-management/role-right-manager"}
             element={<Manager />}
           />
-          
+
           <Route exact path={"/workflows"} element={<Workflow />}>
             <Route
               exact
