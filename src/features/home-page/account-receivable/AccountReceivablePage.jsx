03dda83665cf8292c5542c0239789ea1b3592caa
ali <ali.raza@ceative.co.uk>
2023-06-22 18:51:24 +0500

    Update code home page account receivable

--- src/features/home-page/account-receivable/AccountReceivablePage.jsx
+++ src/features/home-page/account-receivable/AccountReceivablePage.jsx
@@ -1,29 +1,48 @@
 import React from 'react'
 import AcountReceivableCards from './AcountReceivableCards'
-import { Grid, Typography } from '@mui/material'
+import { Grid } from '@mui/material'
 import './receivable.scss'
 import { receivableData } from './data'
 const AccountReceivablePage = () => {
     return (
         <>
             <div className="account__receivable">
-                <Grid container sx={{ mt: '90px' }}>
-                    <Grid fontSize={{md:80,xs:50}} p={{xl:10,  xs:5}} ml={{xl:15}} item xl={6} md={12} xs={12}>
+                <Grid container item spacing={1}
+                    sx={{
+                        mt: '90px',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        gap: '1rem'
+                    }}>
+                    <Grid fontSize={{ md: 55, xs: 40 }}
+                        item xl={3.6}
+                        md={4.8}
+                        xs={6}
+                    >
                         <div className='account_receivable_heading'>
                             Online platform that helps you manage Accounting
                         </div>
                     </Grid>
-                    <Grid  item xl={4} md={12} xs={12} p={{xs:1}} mt={{ xl:'140px'}} ml={{xl: '-100px'}}>
+                    <Grid item
+                        xl={4}
+                        md={6}
+                        xs={10}
+                    >
                         {receivableData?.map((ele) =>
                             <Grid key={ele?.id} container justifyContent={ele?.justifyContent}
-                                m={{ xl: ele?.margin, md: 2, xs:1}} 
-                                item  xs={12} 
+                                margin={{
+                                    xl: ele?.margin,
+                                    md: ele?.margin
+                                }}
+                                item xs={12}
                                 className="receivable__right">
                                 <div className="custom__icon">
-                                    <div className="custom__style" style={{ backgroundColor: ele?.bgColor }}>
+                                    <div className="custom__style"
+                                        style={{ backgroundColor: ele?.bgColor }}>
                                     </div>
                                 </div>
-                                <Grid item className="receivable__text" width={{ xl: ele?.width , md:400, xs: 250}} >
+                                <Grid item className="receivable__text"
+                                    width={{ xl: ele?.width, md: ele?.width, xs: 230 }} >
                                     {ele?.text}
                                 </Grid>
                             </Grid>
@@ -31,7 +50,7 @@ const AccountReceivablePage = () => {
                     </Grid>
                 </Grid>
             </div>
-            <AcountReceivableCards/>
+            <AcountReceivableCards />
         </>
     )
 }
