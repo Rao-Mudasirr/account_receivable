894a3b1c94460a9987ad3c3bacbfbbdeef568537
Muhammad Fahad <muhammad.fahad@ceative.co.uk>
2023-07-10 18:49:50 +0500

    worked on users component

--- src/features/user-management/components/users/users.jsx
+++ src/features/user-management/components/users/users.jsx
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
-import dayjs from 'dayjs';
-import { Card, Box} from '@mui/material';
-import CustomTable from '../../../../components/Table/CustomTable';
-import TableHeader from '../../../../components/Table/TableHeader';
-import { useTableParams } from '../../../../components/Table/useTableParams';
-import TableAction from '../../../../components/Table/TableAction';
-import DeleteModel from '../../../../components/modal/DeleteModel';
-import AddUser from './addusers-input-form/AddUser';
-import EditUser from './editusers-input-form/EditUser';
-import { USERS_DATA } from '.';
-import { useUsers } from './use-user';
+import React, { useState } from "react";
+import dayjs from "dayjs";
+import { Card, Box } from "@mui/material";
+import CustomTable from "../../../../components/Table/CustomTable";
+import TableHeader from "../../../../components/Table/TableHeader";
+import { useTableParams } from "../../../../components/Table/useTableParams";
+import TableAction from "../../../../components/Table/TableAction";
+import DeleteModel from "../../../../components/modal/DeleteModel";
+import EditUser from "./editusers-input-form/EditUser";
+import { USERS_DATA } from ".";
+import { useUsers } from "./use-user";
+import { GlobalSearchBar } from "../../../../components/global-search-filter/global-search-filter";
+import { CustomPopover } from "../../../../components/custom-popover/custom-popover";
+import AddUser from "./addusers-input-form/AddUser";
 
 export const UsersTable = () => {
-
   const {
     open,
     setOpen,
@@ -30,36 +31,37 @@ export const UsersTable = () => {
     tableHeaderRef,
   } = useUsers();
 
-  const { params, headerChangeHandler, pageChangeHandler, sortChangeHandler } = useTableParams();
+  const { params, headerChangeHandler, pageChangeHandler, sortChangeHandler } =
+    useTableParams();
   const columns = [
     {
       accessorFn: (row) => row.Id,
-      id: 'Id',
+      id: "Id",
       cell: (info) => info.getValue(),
       header: () => <span>Id</span>,
     },
     {
       accessorFn: (row) => row.userName,
-      id: 'userName',
+      id: "userName",
       cell: (info) => info.getValue(),
-      header: 'User Name',
+      header: "User Name",
     },
     {
       accessorFn: (row) => row.role,
-      id: 'role',
+      id: "role",
       cell: (info) => info.getValue(),
-      header: 'Role',
+      header: "Role",
     },
     {
       accessorFn: (row) => row.createdOn,
-      id: 'createdOn',
-      cell: (info) => dayjs(info.getValue()).format('DD MMM, YYYY'),
-      header: 'Created On',
+      id: "createdOn",
+      cell: (info) => dayjs(info.getValue()).format("DD MMM, YYYY"),
+      header: "Created On",
     },
     {
-      id: 'Actions',
+      id: "Actions",
       cell: (info) => (
-        <Box sx={{ display: 'flex', gap: '5px', justifyContent: 'center' }}>
+        <Box sx={{ display: "flex", gap: "5px", justifyContent: "center" }}>
           <TableAction type="delete" onClicked={handleOpen} />
           <TableAction type="edit" onClicked={handleFormDialog} />
         </Box>
@@ -71,38 +73,30 @@ export const UsersTable = () => {
 
   return (
     <>
-      <Card sx={{ p: 1 }}>
-        <TableHeader
-          ref={tableHeaderRef}
-          title="Health & Safety"
-          searchKey="search"
-          showAddBtn
-          onAdd={handleFormAddDialog}
-          onChanged={headerChangeHandler}
-        />
+      <Box className='flex justify-space-between'>
+        <GlobalSearchBar />
+        <AddUser/>
+      </Box>
 
-        <CustomTable
-          data={USERS_DATA}
-          columns={columns}
-          onPageChange={pageChangeHandler}
-          onSortByChange={sortChangeHandler}
-          isSuccess={true}
-          isPagination={true}
-        />
-      </Card>
-      <DeleteModel open={open} handleClose={handleClose} onDeleteClick={handleClose} />
+      <CustomTable
+        data={USERS_DATA}
+        columns={columns}
+        onPageChange={pageChangeHandler}
+        onSortByChange={sortChangeHandler}
+        isSuccess={true}
+        isPagination={true}
+      />
+      <DeleteModel
+        open={open}
+        handleClose={handleClose}
+        onDeleteClick={handleClose}
+      />
       <EditUser
         openForm={openForm}
         setOpenForm={setOpenForm}
         handleFormDialog={handleFormDialog}
         handleCloseForm={handleCloseForm}
       />
-        <AddUser
-        openAddForm={openAddForm}
-        setOpenAddForm={setOpenAddForm}
-        handleFormAddDialog={handleFormAddDialog}
-        handleCloseAddForm={handleCloseAddForm}
-        />
     </>
   );
 };
