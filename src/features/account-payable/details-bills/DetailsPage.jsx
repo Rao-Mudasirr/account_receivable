c91a6f4b5ab75b6f253b75e7e5c84e8058f174fd
muhammad kashif <muhammad.kashif@ceative.co.uk>
2023-07-11 23:34:24 +0500

    view button

--- src/features/account-payable/details-bills/DetailsPage.jsx
+++ src/features/account-payable/details-bills/DetailsPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {  useState } from "react";
 import {
   Box,
   Button,
@@ -10,40 +10,136 @@ import {
   InputAdornment,
   Typography
 } from "@mui/material";
+// import './detail.scss';
 import CustomInput from "../../../components/CustomInput";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import Image from "../../../assests/bills/file.png";
+import { ReactComponent as FullScreen } from "../../../assests/bills/maximize.svg";
 import { ReactComponent as Pound } from "../../../assests/bills/pound-sign.svg";
 import CalendarMonthRoundedIcon from "@mui/icons-material/CalendarMonthRounded";
-import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
-import './details.scss'
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
-import AddIcon from '@mui/icons-material/Add';
-import PendingScreen from "../../DocumentApproval/PendingScreen";
 
-const DetailsPage = ({ open, handleClose }) => {
+
+const DetailsPage = ({ open, handleClose ,setOpenModel, openModel}) => {
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
-
-  const [value, setValue] = useState(0);
-  const [openModel, setOpenModel] = useState({
-    model: false,
-    type: 0,
-  });
-
-
   return (
     <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth>
       
        
      
       <DialogContent>
+      <Box className='reuse-details-section'>
+        <Box className="children-first-section">
+          <Box className="image-box">
+            <img src={Image} alt="file" />
+          </Box>
+  
+          {/* <FullScreen
+            className="maximize-btn"
+            onClick={() =>
+              setOpenModel({ ...openModel, model: !openModel.model, type: 1 })
+            }
+          /> */}
+        </Box>
+        <Box className="children-second-section">
+          <Grid container spacing={5} rowSpacing={5}>
+            <Grid item xs={12} md={6} lg={6}>
+              <CustomInput
+                inputClass={"inputClass"}
+                label={"Invoice Number"}
+                required={false}
+                labelClass={"labelClass"}
+              />
+            </Grid>
+            <Grid item xs={12} md={6} lg={3}>
+              <p className="label">Issue Date</p>
+              <LocalizationProvider
+                dateAdapter={AdapterDayjs}
+                style={{ maxWidth: "100%", width: "100%" }}
+              >
+                <DatePicker
+                  sx={{
+                    width: "100%",
+                    ".MuiInputBase-input ": {
+                      p: "13px",
+                      fontFamily: `'Exo 2', "Roboto", "sans-serif"`,
+                      color: startDate ? "#40404D" : "#A6A6B3",
+                    },
+                  }}
+                  slots={{
+                    openPickerIcon: CalendarMonthRoundedIcon,
+                  }}
+                  slotProps={{ textField: { placeholder: "From" } }}
+                  variant="standared"
+                  value={startDate}
+                  onChange={(date) => setStartDate(date)}
+                />
+              </LocalizationProvider>
+            </Grid>
+            <Grid item xs={12} md={6} lg={3}>
+              <p className="label">Bill Due Date</p>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  sx={{
+                    width: "100%",
+                    ".MuiInputBase-input ": {
+                      p: "13px",
+                      fontFamily: `'Exo 2', "Roboto", "sans-serif"`,
+                      color: startDate ? "#40404D" : "#A6A6B3",
+                    },
+                  }}
+                  slots={{
+                    openPickerIcon: CalendarMonthRoundedIcon,
+                  }}
+                  slotProps={{ textField: { placeholder: "From" } }}
+                  variant="standared"
+                  value={endDate}
+                 onChange={(date) => setEndDate(date)}
+                />
+              </LocalizationProvider>
+            </Grid>
+            <Grid item xs={12} md={6} lg={6}>
+              <CustomInput
+                inputClass={"inputClass"}
+                label={"Payment Source"}
+                required={false}
+                labelClass={"labelClass"}
+                type="select"
+              />
+            </Grid>
+            <Grid item xs={12} md={6} lg={6}>
+              <CustomInput
+                inputClass={"inputClass"}
+                label={"Payment Method"}
+                required={false}
+                labelClass={"labelClass"}
+                type="select"
+              />
+            </Grid>
+            <Grid item xs={12} md={6} lg={6}></Grid>
+            <Grid item xs={12} md={6} lg={6}>
+              <CustomInput
+                label={"Payment Amount"}
+                required={false}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <Pound />
+                    </InputAdornment>
+                  ),
+                }}
+                inputClass={"inputClass inputClass2"}
+                labelClass={"labelClass"}
+              />
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
 
-        <PendingScreen/>
+       
       </DialogContent>
-      {/* <DialogActions>
-        <Button onClick={handleClose}>Close</Button>
-      </DialogActions> */}
+     
     </Dialog>
   );
 };
