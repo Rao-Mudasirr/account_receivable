6bfa8888f5d664c31b52bbe0ae476de1d2b47314
Rao Mudasir <raomuhammad.mudasir@ceative.co.uk>
2023-07-13 19:08:07 +0500

    cashflow sidebar

--- src/pages/dashboard/dashboard-cashflow.jsx
+++ src/pages/dashboard/dashboard-cashflow.jsx
@@ -1,9 +1,132 @@
-import React from 'react'
-
+import { Grid } from '@mui/material'
+import React, { useState } from 'react';
+import { dashboardStyle } from "./dashboard-receivable";
+import { DashboardCard } from "../../components/dashboard-card/dashboard-card";
+import { DashboardResponsiveDR } from '../../features/dashboard-responsive-DR/dashboard-responsive-DR';
+import { DashboardToggleIcon } from '../../features/dashboard-toggle-icon/dashboard-toggle-icon';
+import { AgingBucketBarChart } from '../../features/aging-bucket-bar-chart/aging-bucket-bar-chart';
 export const DashboardCashflow = () => {
+  const [agingBucketVal, setAgingBucketVal] = useState("0");
   return (
     <>
-        Cashflow Dashboard
+      <div style={dashboardStyle}>Dashboard</div>
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+          <DashboardCard
+            heading={"Account Receivable"}
+          >
+
+          </DashboardCard>
+        </Grid>
+        <Grid item xs={6}>
+          <DashboardCard
+            heading={"Account Payable"}
+          >
+
+          </DashboardCard>
+        </Grid>
+        <Grid item xs={12}>
+          <DashboardCard
+            heading={"Cash Inflow/Outflow Stats"}
+            headingSibling={<DashboardResponsiveDR />}
+          >
+
+          </DashboardCard>
+        </Grid>
+        <Grid item xs={12}>
+          <DashboardCard
+            heading="Cash Inflow"
+            headingSibling={<DashboardResponsiveDR />}
+          >
+
+          </DashboardCard>
+        </Grid>
+        <Grid item xs={12}>
+          <DashboardCard
+            heading="Cash Outflow"
+            headingSibling={<DashboardResponsiveDR />}
+          >
+
+          </DashboardCard>
+        </Grid>
+        <Grid item xs={12}>
+          <DashboardCard
+            heading="Payment Forecasting"
+            headingSibling={<DashboardResponsiveDR />}
+          >
+
+          </DashboardCard>
+        </Grid>
+        <Grid item xs={12}>
+          <DashboardCard
+            heading="Title"
+          >
+
+          </DashboardCard>
+        </Grid>
+        <Grid item xs={6}>
+          <DashboardCard
+            heading="Invoice Analytics"
+          >
+
+          </DashboardCard>
+        </Grid>
+        <Grid item xs={6}>
+          <DashboardCard
+            heading="Bills Analytics"
+          >
+
+          </DashboardCard>
+        </Grid>
+        <Grid item xs={6}>
+          <DashboardCard
+            heading="Aging Bucket"
+            headingSibling={
+              <div className="flex margin-top-0">
+                <DashboardResponsiveDR />
+                <DashboardToggleIcon
+                  chartSwitch={agingBucketVal}
+                  setSwitch={setAgingBucketVal}
+                />
+              </div>
+            }
+          >
+            <AgingBucketBarChart chartSwitch={agingBucketVal} />
+          </DashboardCard>
+        </Grid>
+        <Grid item xs={6}>
+          <DashboardCard
+            heading="Aging Bucket"
+            headingSibling={
+              <div className="flex margin-top-0">
+                <DashboardResponsiveDR />
+                <DashboardToggleIcon
+                  chartSwitch={agingBucketVal}
+                  setSwitch={setAgingBucketVal}
+                />
+              </div>
+            }
+          >
+            <AgingBucketBarChart chartSwitch={agingBucketVal} />
+          </DashboardCard>
+        </Grid>
+        <Grid item xs={6}>
+          <DashboardCard
+            heading="Top 10 Receivables by Category"
+            headingSibling={<DashboardResponsiveDR />}
+          >
+
+          </DashboardCard>
+        </Grid>
+        <Grid item xs={6}>
+          <DashboardCard
+            heading="Top 10 Payables by Category"
+            headingSibling={<DashboardResponsiveDR />}
+          >
+
+          </DashboardCard>
+        </Grid>
+      </Grid>
     </>
   )
 }
